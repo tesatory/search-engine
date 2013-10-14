c68b1ff7ad831ebce3e7efeae24f9444b86210ff
sainaa <tesatory@yahoo.com>
2013-10-14 12:45:19 -0400

    it is working

--- get_links.py
+++ get_links.py
@@ -1,5 +1,17 @@
 import urllib2
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for attr in attrs:
+                if attr[0] == 'href':
+                    print attr[1]
+                
+
 link = 'http://www.gogo.mn'
 page = urllib2.urlopen(link)
 content = page.read()
-print content
+parser = MyHTMLParser()
+parser.feed(content)
+
